cb68c57f86c78340acf6d481c9104af5026f88e5
Edson Camargo Menezes <dinhocmenezes@hotmail.com>
2019-09-20 18:37:43 -0300

    Santos Futebol Clube - Landing page
    Aprendendo mais sobre flexbox e aproveitando para criar uma landing page em homenagem ao maior brasileiro de todos os tempos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
